8a7ecbe993cd8471faaeb2ec2cb6473bdc4d244d
Oleg Novitskij <o.a.novitskij@gmail.com>
2024-04-14 22:55:09 +0300

    add Pipeline notebook with autorun

---
+++ scripts/utils.py
@@ -0,0 +1,161 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import scipy
+import statsmodels.api as sm
+from joblib import Parallel, delayed
+import os
+import utils
+from sklearn.model_selection import TimeSeriesSplit
+import pandas as pd
+import matplotlib.pyplot as plt
+from typing import Sequence
+import numpy as np
+from scipy import stats
+import statsmodels.api as sm
+import requests
+import json
+from statsmodels.tsa.stattools import adfuller, kpss
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly import graph_objs as go
+from plotly.subplots import make_subplots
+
+plt.rcParams.update({'figure.figsize': (10, 5), 'figure.dpi': 100})
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+from dateutil.parser import parse
+
+from statsmodels.tsa.stattools import adfuller, kpss
+
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+def tsplot(y, lags=None, figsize=(15, 10), style='bmh'):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):    
+        fig = plt.figure(figsize=figsize)
+        #matplotlib.rcParams['font.family'] = 'Ubuntu Mono'
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+        
+        y.plot(ax=ts_ax)
+        ts_ax.set_title('Time Series Analysis Plots')
+        
+        sm.graphics.tsa.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.05)
+        sm.graphics.tsa.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.05)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')        
+        scipy.stats.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return fig
+        
+
+def plot_decomposition(y, decompose_type: str = "additive"):
+    result_add = seasonal_decompose(y, model=decompose_type, extrapolate_trend='freq')
+
+    # Plot
+    plt.rcParams.update({'figure.figsize': (10,10)})
+    fig = result_add.plot()
+    
+    plt.rcParams.update({'figure.figsize': (10,5)})
+    return fig
+
+def plotly_df(df, title = ''):
+
+    fig = make_subplots(rows=df.shape[1], cols=1)
+    counter = 1
+    
+    for column in df.columns:
+        trace = go.Scatter(
+            x = df.index,
+            y = df[column],
+            mode = 'lines',
+            name = column
+        )
+        fig.add_trace(trace,
+                      col=1,
+                      row=counter)
+        counter += 1
+
+    layout = dict(title = title)
+    fig.update_layout(layout)
+    iplot(fig, show_link=False)
+    
+def plotMovingAverage(series, n):
+
+    """
+    series - dataframe with timeseries
+    n - rolling window size 
+
+    """
+
+    rolling_mean = series.rolling(window=n).mean()
+
+    # При желании, можно строить и доверительные интервалы для сглаженных значений
+    #rolling_std =  series.rolling(window=n).std()
+    #upper_bond = rolling_mean+1.96*rolling_std
+    #lower_bond = rolling_mean-1.96*rolling_std
+
+    plt.figure(figsize=(15,5))
+    plt.title("Moving average\n window size = {}".format(n))
+    plt.plot(rolling_mean, "g", label="Rolling mean trend")
+
+    #plt.plot(upper_bond, "r--", label="Upper Bond / Lower Bond")
+    #plt.plot(lower_bond, "r--")
+    plt.plot(dataset[n:], label="Actual values")
+    plt.legend(loc="upper left")
+    plt.grid(True)
+    
+def weighted_average(series: Sequence, weights: Sequence):
+    '''
+    Calculates weighted cumulative sum for the series
+    Note: weights MUST sum to 1
+    '''
+    result = 0.0
+    weights.reverse()
+    for n in range(len(weights)):
+        result += series[-n-1] * weights[n]
+    return result
+
+
+
+def timeseriesCVscore(x):
+    # вектор ошибок
+    errors = []
+
+    values = data.values
+    alpha, beta, gamma = x
+
+    # задаём число фолдов для кросс-валидации
+    tscv = TimeSeriesSplit(n_splits=3) 
+
+    # идем по фолдам, на каждом обучаем модель, строим прогноз на отложенной выборке и считаем ошибку
+    for train, test in tscv.split(values):
+
+        model = HoltWinters(series=values[train], slen = 24*7, alpha=alpha, beta=beta, gamma=gamma, n_preds=len(test))
+        model.triple_exponential_smoothing()
+
+        predictions = model.result[-len(test):]
+        actual = values[test]
+        error = mean_squared_error(predictions, actual)
+        errors.append(error)
+
+    # Возвращаем средний квадрат ошибки по вектору ошибок 
+    return np.mean(np.array(errors))
+
+def stationarity_check(target)
+    result = adfuller(target, autolag='AIC')
+
+    print(f'ADF Statistic: {result[0]}')
+    print(f'p-value: {result[1]}')
+    for key, value in result[4].items():
+        print('Critial Values:')
+        print(f'   {key}, {value}')
